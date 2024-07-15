c6ef96026b265d99c64056396c01d68f790b28a4
Fahri Kurniawan <111134259+fahrikurniawan99@users.noreply.github.com>
2024-07-15 23:26:50 +0700

    add overview in storybook

---
+++ storybook-static/assets/Alert.stories-Dj5DT_MN.js
@@ -0,0 +1,25 @@
+import{A as w}from"./Button-DCdkl-Ie.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const M={component:w},s={args:{message:"This is an alert!"}},r={args:{message:"This is an alert!",color:"error"}},e={args:{message:"This is an alert!",color:"success"}},a={args:{message:"This is an alert!",color:"info"}},o={args:{message:"This is an alert!",color:"warning"}};var n,c,t;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    message: "This is an alert!"
+  }
+}`,...(t=(c=s.parameters)==null?void 0:c.docs)==null?void 0:t.source}}};var i,m,g;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    message: "This is an alert!",
+    color: "error"
+  }
+}`,...(g=(m=r.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var p,l,u;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    message: "This is an alert!",
+    color: "success"
+  }
+}`,...(u=(l=e.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var d,h,T;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    message: "This is an alert!",
+    color: "info"
+  }
+}`,...(T=(h=a.parameters)==null?void 0:h.docs)==null?void 0:T.source}}};var S,f,E;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    message: "This is an alert!",
+    color: "warning"
+  }
+}`,...(E=(f=o.parameters)==null?void 0:f.docs)==null?void 0:E.source}}};const W=["Main","Error","Success","Info","Warning"];export{r as Error,a as Info,s as Main,e as Success,o as Warning,W as __namedExportsOrder,M as default};
